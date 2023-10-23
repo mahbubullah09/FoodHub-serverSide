44a1f45d5565ed34fa15b11a9b5e21ed0da665b2
Mahbubullah <mahbubullahpathan@gmail.com>
2023-10-24 01:13:59 +0600

    single data fetch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,15 @@ async function run() {
 app.get('/products', async(req,res)=>{
     const cursor = productsCollection.find();
     const result = await cursor.toArray();
+    res.send(result);
+})
+app.get('/products/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+
+    
+    const result = await productsCollection.findOne(query);
+    
     res.send(result);
 })
 
