900993e83508484551baa23e34541fa7a68021c8
Mahbubullah <mahbubullahpathan@gmail.com>
2023-10-23 06:31:05 +0600

    server init

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('foodHub server is running')
+})
+
+app.listen(port, () => {
+    console.log(`foodHub Server is running on port: ${port}`)
+})
